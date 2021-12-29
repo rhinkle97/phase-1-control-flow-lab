8212c6c7e530a1ab31024909a57038d18ddcf74e
Ryan Hinkle <hinkle.ryan97@gmail.com>
2021-12-29 12:50:22 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result
+  if (ride <= 400) {
+    result = 'This one is on me!';
+  }
+  if (ride > 2000) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  if (ride > 2500) {
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let result
+  if (city === 'NYC') {
+    result = 'Ok, sounds good.';
+  }
+  else result = 'No go.';
+
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result
+  if (tip === 'generous') {
+    result = 'Thank you so much.';
+  }
+  else if (tip === 'not as generous') {
+    result = 'Thank you.';
+  }
+  else if (tip === 'thanks for everything') {
+      result = 'Bye.';
+    }
+  return result;
 }
